32c330b92bb19eb5d949878310ac9110369ddbad
Yevhen <terrokt@gmail.com>
2022-04-07 00:07:45 +0200

    make a hover effects

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-let swiper = new Swiper('.slider-container', {
+const swiper1 = document.querySelector('.slider-container'),
+      burger = document.querySelector('.burger'),
+      close = document.querySelector('.menu__close'),
+      menu = document.querySelector('.menu')
+
+let swiper = new Swiper(swiper1, {
   centeredSlides: true,
   slidesPerView: 'auto',
   loop: true,
   spaceBetween: 105,
+});
+
+burger.addEventListener('click',() => {
+  menu.classList.add('menu--visible');
+});
+
+close.addEventListener('click',() => {
+  menu.classList.remove('menu--visible');
 });
